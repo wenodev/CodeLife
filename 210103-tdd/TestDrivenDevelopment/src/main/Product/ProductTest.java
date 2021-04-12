25750c945f03850d45f8d7e7f4821417bdb752ab
wenodev <logicalstark1105@gmail.com>
2021-04-12 22:28:14 +0900

    test: Product 생성자에 price를 추가하고 실패하라

--- 210103-tdd/TestDrivenDevelopment/src/main/Product/ProductTest.java
+++ 210103-tdd/TestDrivenDevelopment/src/main/Product/ProductTest.java
@@ -1,7 +1,15 @@
 package main.Product;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+/*
+1. 상품의 가격을 추가하라
+*/
 class ProductTest {
+
+    @Test
+    void testProduct(){
+        Product product = new Product(1000);
+    }
 
 }
