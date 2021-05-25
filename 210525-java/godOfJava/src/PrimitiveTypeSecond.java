f9ac724f5b2f818d73b3ea859bb8da3af5bf5c21
wenodev <logicalstark1105@gmail.com>
2021-05-25 20:19:15 +0900

    feat: 기본값 확인하는 코드 작성하라

---
+++ 210525-java/godOfJava/src/PrimitiveTypeSecond.java
@@ -0,0 +1,13 @@
+public class PrimitiveTypeSecond {
+
+    int intDefault;
+    byte byteDefault;
+
+    public static void main(String[] args) {
+
+        PrimitiveTypeSecond typeSecond = new PrimitiveTypeSecond();
+
+        System.out.println(typeSecond.intDefault);
+        System.out.println(typeSecond.byteDefault);
+    }
+}
