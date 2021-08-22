d2ea528ece7b826fd1f54f734b320b119eae851a
= <=>
2021-08-22 23:55:53 +0900

    feat: String docs 확인하라

---
+++ 210525-java/godOfJava/src/ch15/Basic.java
@@ -0,0 +1,9 @@
+package ch15;
+
+public class Basic {
+    public static void main(String[] args) {
+        String name = "";
+        StringBuilder nameBuilder = new StringBuilder();
+        StringBuffer nameBuffer = new StringBuffer();
+    }
+}
