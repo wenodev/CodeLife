b5be930e461e10d3e936d0ad6ee405c195745086
= <=>
2021-08-23 00:02:08 +0900

    git test

--- 210525-java/godOfJava/src/ch15/Basic.java
+++ 210525-java/godOfJava/src/ch15/Basic.java
@@ -2,6 +2,7 @@ package ch15;
 
 public class Basic {
     public static void main(String[] args) {
+        // git test
         String name = "";
         StringBuilder nameBuilder = new StringBuilder();
         StringBuffer nameBuffer = new StringBuffer();
