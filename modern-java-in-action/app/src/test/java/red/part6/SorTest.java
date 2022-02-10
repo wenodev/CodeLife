443470688d6c6609279990d407e83d7c458c922e
wenodev <logicalstark1105@gmail.com>
2022-02-10 20:35:31 +0900

    feat: 1주차 스터디 완료

--- modern-java-in-action/app/src/test/java/red/part6/SorTest.java
+++ modern-java-in-action/app/src/test/java/red/part6/SorTest.java
@@ -2,6 +2,7 @@ package red.part6;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,4 +26,64 @@ public class SorTest {
 
         assertThat(stringList).containsExactly("anny", "james", "sonny");
     }
+
+    // 클래스 옮기기
+
+    @Test
+    void test_limit(){
+        List<String> result = List.of("a", "b", "c").stream().limit(2).collect(Collectors.toList());
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void test_skip(){
+        List<String> result = List.of("a", "b", "c").stream().skip(2).collect(Collectors.toList());
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    void test_min(){
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        Integer result =  numbers.stream().max(Integer::compareTo).orElse(0);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void test_max(){
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        Integer result =  numbers.stream().min(Integer::compareTo).orElse(0);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void test_count(){
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        Long result =  numbers.stream().filter(n -> n < 3).count();
+        assertThat(result).isEqualTo(2L);
+    }
+
+    @Test
+    void test_allMatch(){
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        boolean result = numbers.stream().allMatch(n -> n >= 0);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void test_anyMatch(){
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        boolean result = numbers.stream().anyMatch(n -> n >= 3);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void test_findAnyAndFindFirst(){
+        List<Integer> numbers = Arrays.asList(1,2,3);
+        int anyResult1 = numbers.stream().filter(n -> n >= 0).findAny().orElse(0);
+        int anyResult2 = numbers.stream().filter(n -> n >= 0).findAny().orElse(0);
+
+        assertThat(anyResult1).isEqualTo(1);
+        assertThat(anyResult2).isEqualTo(1);
+    }
+
 }
--- modern-java-in-action/app/src/test/java/red/part7/OptionalTest.java
+++ modern-java-in-action/app/src/test/java/red/part7/OptionalTest.java
@@ -1,2 +1,68 @@
-package red.part7;public class OptionalTest {
+package red.part7;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+class User{
+    private Integer id;
+    private String name;
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+public class OptionalTest {
+    User user1 = new User(5, "james");
+    User user2 = new User(2, "sonny");
+    User user3 =new User(3 );
+
+    @Test
+    void test_init(){
+        Optional.of(user2.getName());
+        Optional.ofNullable(user3);
+        Optional.empty();
+    }
+
+    @Test
+    void test_orElse(){
+        Optional.of(user3.getName()).orElse("defaultName");
+        Optional.of(user3.getName()).orElseGet(() -> "defaultName");
+        Optional.of(user3.getName()).orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    @Test
+    void test_ifPresent_with_null(){
+        Optional<User> user = Optional.ofNullable(null);
+        user.ifPresent(u -> System.out.println(u.getName()));
+    }
+
+    @Test
+    void test_ifPresent_with_non_null(){
+        Optional<User> user = Optional.ofNullable(new User(1, "weno"));
+        user.ifPresent(u -> System.out.println(u.getName()));
+    }
+
 }
