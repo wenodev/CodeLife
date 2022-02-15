ead98dfa8656489a80c061c26717f4c809635fa9
wenodev <logicalstark1105@gmail.com>
2022-02-16 08:02:01 +0900

    feat: 클로저 테스트

--- modern-java-in-action/app/src/test/java/red/part9/ClosureTest.java
+++ modern-java-in-action/app/src/test/java/red/part9/ClosureTest.java
@@ -1,2 +1,25 @@
-package red.part9;public class ClosureTest {
+package red.part9;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClosureTest {
+
+    @Test
+    void test_closure(){
+        Supplier<String> supplier = getStringSupplier();
+        assertThat(supplier.get()).isEqualTo("HelloWorld");
+    }
+
+    private Supplier<String> getStringSupplier() {
+        String hello = "Hello";
+        Supplier<String> supplier = () -> {
+            String world = "World";
+            return hello + world;
+        };
+        return supplier;
+    }
 }
